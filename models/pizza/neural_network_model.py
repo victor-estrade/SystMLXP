aaf67388b36125be8cb8fc3c1de45c20afb7f050
Victor Estrade <estrade@lri.fr>
2018-02-05 14:27:29 +0100

    Add Augmented models

--- models/pizza/neural_network_model.py
+++ models/pizza/neural_network_model.py
@@ -13,10 +13,11 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.preprocessing import StandardScaler
 from sklearn.externals import joblib
 
-from .net.neural_net import NeuralNetClassifier
+from ..net.neural_net import NeuralNetClassifier
+from ..net.weighted_criterion import WeightedCrossEntropyLoss
 
-from .net.weighted_criterion import WeightedCrossEntropyLoss
 from .architecture import Net
+from ..data_augment import NormalDataAugmenter
 
 class NeuralNetModel(BaseEstimator, ClassifierMixin):
     def __init__(self, n_steps=5000, batch_size=20, learning_rate=1e-3, cuda=False, verbose=0):
@@ -38,7 +39,7 @@ class NeuralNetModel(BaseEstimator, ClassifierMixin):
 
     def fit(self, X, y, sample_weight=None):
         X = self.scaler.fit_transform(X)
-        self.clf.fit(X, y)
+        self.clf.fit(X, y, sample_weight=sample_weight)
         return self
     
     def predict(self, X):
@@ -77,3 +78,72 @@ class NeuralNetModel(BaseEstimator, ClassifierMixin):
     def get_name(self):
         name = "NeuralNetModel-{}-{}-{}".format(self.n_steps, self.batch_size, self.learning_rate)
         return name
+
+
+class AugmentedNeuralNetModel(BaseEstimator, ClassifierMixin):
+    def __init__(self, skewing_function, n_steps=5000, batch_size=20, learning_rate=1e-3, width=1, n_augment=2,
+                 cuda=False, verbose=0):
+        super().__init__()
+        self.n_steps = n_steps
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.width = width
+        self.n_augment = n_augment
+        self.cuda = cuda
+        self.verbose = verbose
+        
+        self.net = Net()
+        
+        self.optimizer = optim.Adam(self.net.parameters(), lr=learning_rate)
+        self.criterion = WeightedCrossEntropyLoss()
+        
+        self.augmenter = NormalDataAugmenter(skewing_function, width=width, n_augment=n_augment)
+
+        self.scaler = StandardScaler()
+        self.clf = NeuralNetClassifier(self.net, self.criterion, self.optimizer, 
+                                       n_steps=self.n_steps, batch_size=self.batch_size)
+
+    def fit(self, X, y, sample_weight=None):
+        X, y, sample_weight = self.augmenter(X, y, sample_weight)
+        X = self.scaler.fit_transform(X)
+        self.clf.fit(X, y, sample_weight=sample_weight)
+        return self
+    
+    def predict(self, X):
+        X = self.scaler.transform(X)
+        y_pred = self.clf.predict(X)
+        return y_pred
+    
+    def predict_proba(self, X):
+        X = self.scaler.transform(X)
+        proba = self.clf.predict_proba(X)
+        return proba
+
+    def save(self, dir_path):
+        path = os.path.join(dir_path, 'weights.pth')
+        torch.save(self.net.state_dict(), path)
+        
+        path = os.path.join(dir_path, 'Scaler.pkl')
+        joblib.dump(self.scaler, path)
+        return self
+    
+    def load(self, dir_path):
+        path = os.path.join(dir_path, 'weights.pth')
+        if self.cuda:
+            self.net.load_state_dict(torch.load(path))
+        else:
+            self.net.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage))
+
+        path = os.path.join(dir_path, 'Scaler.pkl')
+        self.scaler = joblib.load(path)
+        return self
+
+    def describe(self):
+        return dict(name='augmented_neural_net', learning_rate=self.learning_rate, 
+                    n_steps=self.n_steps, batch_size=self.batch_size, width=self.width, n_augment=self.n_augment)
+        
+    def get_name(self):
+        name = "AugmentedNeuralNetModel-{}-{}-{}-{}-{}".format(self.n_steps, self.batch_size, self.learning_rate,
+                        self.width, self.n_augment)
+        return name
+
--- models/pizza/tangent_prop_model.py
+++ models/pizza/tangent_prop_model.py
@@ -15,11 +15,12 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.preprocessing import StandardScaler
 from sklearn.externals import joblib
 
-from .net.tangent_prop import TangentPropClassifier
-from .net.weighted_criterion import WeightedCrossEntropyLoss
-from .tangent_extract import TangentExtractor
+from ..net.tangent_prop import TangentPropClassifier
+from ..net.weighted_criterion import WeightedCrossEntropyLoss
+from ..tangent_extract import TangentExtractor
 
 from .architecture import JNet
+from ..data_augment import NormalDataAugmenter
 
 class TangentComputer(object):
     """ For 2D rotation """
@@ -65,7 +66,83 @@ class TangentPropModel(BaseEstimator, ClassifierMixin):
     def fit(self, X, y, sample_weight=None):
         T = self.tangent_extractor.compute_tangent(X)
         X = self.scaler.fit_transform(X)
-        self.clf.fit(X, y, T)
+        self.clf.fit(X, y, T, sample_weight=sample_weight)
+        return self
+    
+    def predict(self, X):
+        X = self.scaler.transform(X)
+        y_pred = self.clf.predict(X)
+        return y_pred
+    
+    def predict_proba(self, X):
+        X = self.scaler.transform(X)
+        proba = self.clf.predict_proba(X)
+        return proba
+
+    def save(self, dir_path):
+        path = os.path.join(dir_path, 'weights.pth')
+        torch.save(self.jnet.state_dict(), path)
+        
+        path = os.path.join(dir_path, 'Scaler.pkl')
+        joblib.dump(self.scaler, path)
+        return self
+    
+    def load(self, dir_path):
+        path = os.path.join(dir_path, 'weights.pth')
+        if self.cuda:
+            self.jnet.load_state_dict(torch.load(path))
+        else:
+            self.jnet.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage))
+
+        path = os.path.join(dir_path, 'Scaler.pkl')
+        self.scaler = joblib.load(path)
+        return self
+    
+    def describe(self):
+        return dict(name='tangent_prop', n_steps=self.n_steps, batch_size=self.batch_size, 
+                    learning_rate=self.learning_rate, trade_off=self.trade_off, alpha=self.alpha)
+    
+    def get_name(self):
+        name = "TangentPropModel-{}-{}-{}-{}-{}".format(self.n_steps, self.batch_size, 
+                            self.learning_rate, self.trade_off, self.alpha)
+        return name
+
+
+class AugmentedTangentPropModel(BaseEstimator, ClassifierMixin):
+    def __init__(self, skewing_function, n_steps=5000, batch_size=20, learning_rate=1e-3, trade_off=1, alpha=1e-2, 
+                    width=1, n_augment=2, cuda=False, verbose=0):
+        super().__init__()
+        self.skewing_function = skewing_function
+        self.n_steps = n_steps
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.trade_off = trade_off
+        self.alpha = alpha
+        self.width = width
+        self.n_augment = n_augment
+        self.cuda = cuda
+        self.verbose = verbose
+        
+        self.jnet = JNet()
+        
+        self.optimizer = optim.Adam(self.jnet.parameters(), lr=learning_rate)
+        self.criterion = WeightedCrossEntropyLoss()
+        
+        self.tangent_extractor = TangentExtractor(skewing_function, alpha=alpha)
+#         self.tangent_extractor = TangentComputer()
+
+        self.augmenter = NormalDataAugmenter(skewing_function, width=width, n_augment=n_augment)
+
+        self.scaler = StandardScaler()
+        self.clf = TangentPropClassifier(self.jnet, self.criterion, self.optimizer, 
+                                         n_steps=self.n_steps, batch_size=self.batch_size,
+                                         trade_off=trade_off)
+
+    def fit(self, X, y, sample_weight=None):
+        X, y, sample_weight = self.augmenter(X, y, sample_weight)
+        T = self.tangent_extractor.compute_tangent(X)
+        X = self.scaler.fit_transform(X)
+        self.clf.fit(X, y, T, sample_weight=sample_weight)
         return self
     
     def predict(self, X):
