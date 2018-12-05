856c34061f71e5556b0053a0ded325bc1f1dabbc
Victor Estrade <victor.estrade@lri.fr>
2018-12-05 17:38:51 +0100

    Fix missing numpy includes

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 # coding: utf-8
 from __future__ import division
 
+import numpy as np
+
 from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    name = "Cython facilities",
+    name = "Merging intertwinged",
     ext_modules = cythonize('merging.pyx'),  # accepts a glob pattern
-)
+    include_dirs = [np.get_include()],
+    )
